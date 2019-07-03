91dbad8076faf6b12e96941b1b3b9e7cb933bf48
Samuli Asmala <samuli.asmala@gmail.com>
2019-07-03 19:42:58 +0300

    Initial commit

---
+++ .bash_aliases
@@ -0,0 +1,21 @@
+# Add custom script directory to path
+if [ -d "$HOME/bin" ] ; then
+    export PATH="$HOME/bin:$PATH"
+fi
+
+# Start typing part of a command and up/down arrows will only jump to those
+# commands in the history which start with the same fragment of a command.
+bind '"\e[A":history-search-backward'
+bind '"\e[B":history-search-forward'
+
+# some more ls aliases
+alias ls='ls --color=auto -F -h'
+alias ll='ls -l'
+alias la='ls -A'
+alias l='ls'
+alias g='git'
+alias diff='diff --color=auto'
+alias dfd='df -h | grep -e "/dev/sd" -e "Filesystem" --color=never'
+
+# Command aliases
+alias o='xdg-open'
---
+++ bootstrap.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+DOTFILES=(
+    .bashrc
+    .bash_aliases
+    .gitconfig
+    .screenrc
+)
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+read -p 'Set timezone to Finland? (y/n) ' -n 1
+echo ''
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+  sudo timedatectl set-timezone Europe/Helsinki
+fi
+
+read -p 'Overwrite current dotfiles? (y/n) ' -n 1 OVERWRITE
+echo ''
+if [[ ! $OVERWRITE =~ ^[Yy]$ ]]; then
+  echo 'Current dotfiles are renamed to .bak'
+fi
+
+for i in "${DOTFILES[@]}"; do
+  read -p "Symlink $i? (y/n) " -n 1
+  echo ''
+  if [[ $REPLY =~ ^[Yy]$ ]]; then
+    if [ -f "$HOME/$i" ]; then
+      if [[ $OVERWRITE =~ ^[Nn]$ ]]; then
+        mv -v "$HOME/$i" "$HOME/$i.bak"
+      else
+        rm -v "$HOME/$i"
+      fi
+    fi
+    ln -sv "$SCRIPT_DIR/$i" "$HOME/$i"
+  fi
+done
+
+read -p "Create local socket directory for screen to $HOME/.screen? (y/n) " -n 1
+echo ''
+if [[ $REPLY =~ ^[Yy]$ ]] && [ ! -d $HOME/.screen ]; then
+  mkdir -v "$HOME/.screen"
+fi
